2392ebf31e8567d6bb6e7f70ac8509b91f59fe1c
limos-github <lib97391459@gmail.com>
2020-10-15 18:11:46 +0800

    初次提交为一个空的springboot项目

---
+++ src/main/java/com/limo/lb/LearningBestApplication.java
@@ -0,0 +1,13 @@
+package com.limo.lb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningBestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningBestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/limo/lb/LearningBestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limo.lb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningBestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
