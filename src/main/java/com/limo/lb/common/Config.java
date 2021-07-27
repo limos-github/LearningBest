4a3396d2354071a4d9ef43ad91e1fac478887310
libiao <libiao@jtexpress.com.cn>
2021-07-27 10:25:14 +0800

    1.测试git

--- src/main/java/com/limo/lb/common/Config.java
+++ src/main/java/com/limo/lb/common/Config.java
@@ -2,4 +2,5 @@ package com.limo.lb.common;
 
 public class Config {
     private String property;
+    private String propertis;
 }
--- src/main/java/com/limo/lb/controller/HelloWorld.java
+++ src/main/java/com/limo/lb/controller/HelloWorld.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorld {
     private String property;
+    private String propertis;
     @RequestMapping("/hello")
     public String hello(){
         return "hello world!";
